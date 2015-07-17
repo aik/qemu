865acb41e008adcf648da4246eecd9e6f34381b2
Alexey Kardashevskiy <aik@ozlabs.ru>
2015-07-17 16:42:54 +1000

    memory: Add reporting of supported page sizes
    Every IOMMU has some granularity which MemoryRegionIOMMUOps::translate
    uses when translating, however this information is not available outside
    the translate context for various checks.
    
    This adds a get_page_sizes callback to MemoryRegionIOMMUOps and
    a wrapper for it so IOMMU users (such as VFIO) can know the actual
    page size(s) used by an IOMMU.
    
    The qemu_real_host_page_mask is used as fallback.
    
    Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>
    ---
    Changes:
    v4:
    * s/1<<TARGET_PAGE_BITS/qemu_real_host_page_size/

--- hw/ppc/spapr_iommu.c
+++ hw/ppc/spapr_iommu.c
@@ -104,6 +104,13 @@ static IOMMUTLBEntry spapr_tce_translate_iommu(MemoryRegion *iommu, hwaddr addr,
     return ret;
 }
 
+static uint64_t spapr_tce_get_page_sizes(MemoryRegion *iommu)
+{
+    sPAPRTCETable *tcet = container_of(iommu, sPAPRTCETable, iommu);
+
+    return 1ULL << tcet->page_shift;
+}
+
 static int spapr_tce_table_post_load(void *opaque, int version_id)
 {
     sPAPRTCETable *tcet = SPAPR_TCE_TABLE(opaque);
@@ -135,6 +142,7 @@ static const VMStateDescription vmstate_spapr_tce_table = {
 
 static MemoryRegionIOMMUOps spapr_iommu_ops = {
     .translate = spapr_tce_translate_iommu,
+    .get_page_sizes = spapr_tce_get_page_sizes,
 };
 
 static int spapr_tce_table_realize(DeviceState *dev)
