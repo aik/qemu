72f6a9fd46708bddc5bc8d2e5d31e502aa882734
Alexey Kardashevskiy <aik@ozlabs.ru>
2017-10-16 16:20:04 +1100

    ppc/spapr: Receive and store device tree blob from SLOF
    SLOF receives a device tree and updates it with various properties
    before switching to the guest kernel and QEMU is not aware of any changes
    made by SLOF. Since there is no real RTAS and QEMU implements it,
    it makes sense to pass the SLOF device tree to QEMU so the latter could
    implement RTAS related tasks better.
    
    Specifially, now QEMU can find out the actual XICS phandle (for PHB
    hotplug) and the RTAS linux,rtas-entry/base properties (for firmware
    assisted NMI - FWNMI).
    
    This stores the initial DT blob in the sPAPR machine and replaces it
    in the KVMPPC_H_UPDATE_DT (new private hypercall) handler.
    
    This implements a basic FDT header validity check of the new blob;
    the new blob size should not increase more than twice since the reset.
    
    This adds an @update_dt_enabled machine property to allow backward
    migration.
    
    Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>

--- hw/ppc/spapr_hcall.c
+++ hw/ppc/spapr_hcall.c
@@ -1717,6 +1717,45 @@ static target_ulong h_get_cpu_characteristics(PowerPCCPU *cpu,
 
     args[0] = characteristics;
     args[1] = behaviour;
+    return H_SUCCESS;
+}
+
+static target_ulong h_update_dt(PowerPCCPU *cpu, sPAPRMachineState *spapr,
+                                target_ulong opcode, target_ulong *args)
+{
+    target_ulong dt = ppc64_phys_to_real(args[0]);
+    struct fdt_header hdr = { 0 };
+    unsigned cb;
+    sPAPRMachineClass *smc = SPAPR_MACHINE_GET_CLASS(spapr);
+
+    cpu_physical_memory_read(dt, &hdr, sizeof(hdr));
+    cb = fdt32_to_cpu(hdr.totalsize);
+
+    if (fdt_check_header(&hdr) ||
+        fdt32_to_cpu(hdr.boot_cpuid_phys) ||
+        fdt32_to_cpu(hdr.off_dt_struct) >= cb ||
+        fdt32_to_cpu(hdr.off_dt_strings) >= cb ||
+        fdt32_to_cpu(hdr.off_mem_rsvmap) >= cb ||
+        fdt32_to_cpu(hdr.totalsize) != fdt32_to_cpu(hdr.size_dt_strings) +
+                fdt32_to_cpu(hdr.size_dt_struct) +
+                sizeof(struct fdt_reserve_entry) +
+                sizeof(hdr) ||
+        cb / spapr->fdt_initial_size > 2) {
+        trace_spapr_update_dt_failed(spapr->fdt_initial_size, cb,
+            fdt32_to_cpu(hdr.magic));
+        return H_PARAMETER;
+    }
+
+    if (!smc->update_dt_enabled) {
+        return H_SUCCESS;
+    }
+
+    g_free(spapr->fdt_blob);
+    spapr->fdt_size = cb;
+    spapr->fdt_blob = g_malloc0(cb);
+    cpu_physical_memory_read(dt, spapr->fdt_blob, cb);
+
+    trace_spapr_update_dt(cb);
 
     return H_SUCCESS;
 }
@@ -1822,6 +1861,8 @@ static void hypercall_register_types(void)
 
     /* ibm,client-architecture-support support */
     spapr_register_hypercall(KVMPPC_H_CAS, h_client_architecture_support);
+
+    spapr_register_hypercall(KVMPPC_H_UPDATE_DT, h_update_dt);
 }
 
 type_init(hypercall_register_types)
