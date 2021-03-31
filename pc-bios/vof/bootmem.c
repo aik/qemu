b9609dcca0f2532417cc268b868db17fc4bd8ae0
Alexey Kardashevskiy <aik@ozlabs.ru>
2021-03-31 13:13:07 +1100

    vof: Add printk
    Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>

--- pc-bios/vof/bootmem.c
+++ pc-bios/vof/bootmem.c
@@ -10,5 +10,6 @@ void boot_from_memory(uint64_t initrd, uint64_t initrdsize)
         return;
     }
 
+    printk("Booting from memory @0x%lx\n", kern[0]);
     do_boot(kern[0], initrd, initrdsize);
 }
